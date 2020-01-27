8d6eb0971773b66758667a75af6f6dc07d6f41db
Vadim <korobeinikovv@mail.ru>
2020-01-27 17:35:37 +0300

    Updated test app code

---
+++ app/inMemoryStorage.js
@@ -0,0 +1,17 @@
+var storage = {};
+
+var inMemoryStorage = {
+  getItem: key => {
+    return storage[key];
+  },
+  setItem: (key, value) => {
+    storage[key] = value;
+    console.log('Storage', storage);
+  },
+  removeItem: key => {
+    delete storage[key];
+    console.log('Storage', storage);
+  },
+};
+
+module.exports = inMemoryStorage;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,33 @@
 var BaAuthApiClient = require('../dist/ba-auth-api-client');
+var inMemoryStorage = require('./inMemoryStorage');
 
-var URL = 'https://localhost';
-var client = new BaAuthApiClient(URL);
+var URL = 'http://127.0.0.1:5011';
+var client = new BaAuthApiClient(URL, {
+  storage: inMemoryStorage,
+});
 
-try {
-  client.refreshToken();
-  console.log('Response', client.refreshToken());
-} catch (e) {
-  console.log('Error');
-}
+// try {
+//   client.refreshToken();
+//   console.log('Response', client.refreshToken());
+// } catch (e) {
+//   console.log('Error');
+// }
+
+var done = false;
+
+(function wait() {
+  if (!done) setTimeout(wait, 1000);
+})();
+
+// for testing purposes
+global.fetch = require('node-fetch');
+
+client
+  .login('ba-user-1', 'ba-password')
+  .then(res => {
+    console.log(res);
+    done = true;
+  })
+  .catch(e => {
+    console.log('Error', e);
+  });
