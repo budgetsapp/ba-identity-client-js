ce5935181faca4797b451cad4269be5c40b64877
Vadim <korobeinikovv@mail.ru>
2020-01-28 11:10:27 +0300

    Added jsdoc to token

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,10 +1,20 @@
 import jwt_decode from 'jwt-decode';
 import { secToMs } from '../utils/time';
 
+/**
+ * Decodes JWT token
+ * @param    {string} token - JWT access token or JWT refresh token
+ * @returns  {Object} object containing claims and other data
+ */
 export function parseJwt(token) {
   return jwt_decode(token);
 }
 
+/**
+ * Returns custom object from JWT access token
+ * @param    {string} accessToken - JWT access token
+ * @returns  {Object} flatten custom object containg data from decoded JWT token
+ */
 export function extractDataFromToken(accessToken) {
   const parsedToken = parseJwt(accessToken);
   return {
