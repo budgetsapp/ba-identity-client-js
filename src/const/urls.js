d866e457d1d5cbbdc679477bbb5a1bb4862c08e9
Vadim <korobeinikovv@mail.ru>
2020-02-09 18:41:39 +0300

    Renamed endpoints

--- src/const/urls.js
+++ src/const/urls.js
@@ -1,2 +1,2 @@
-export const GET_TOKENS_URL = '/get-tokens';
-export const REFRESH_TOKEN_URL = '/refresh-token';
+export const GET_TOKENS_URL = '/v1/signin';
+export const REFRESH_TOKEN_URL = '/v1/refresh';
